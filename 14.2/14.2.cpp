84e02fbd1b147a969a6f5851cd69235f21080fe5
Dima <dimaa17.fl@gmail.com>
2023-12-16 17:17:54 +0200

    Добавьте файлы проекта.

---
+++ 14.2/14.2.cpp
@@ -0,0 +1,34 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+#include <stdlib.h>
+#define size  6
+
+
+int main() {
+	int arr[size];
+	int i = 0;
+	int* pokaz;
+	int sum = 0;
+	float res;
+
+	pokaz = &arr[0];
+	for (i = 0; i < size; i++) {
+
+		printf("Vvedit %d element masivu: ", i + 1);
+		scanf("%d", (pokaz + i));
+
+		sum = sum + (*(pokaz + i));
+
+	}
+
+	res = sum / size;
+
+	for (i = 0; i < size; i++) {
+		printf("%d elementu masivu: %d\n", i + 1, (*(pokaz + i)));
+
+	}
+	printf("seredne znachennya elementiv masivu: %2.f", res);
+
+
+	return 0;
+}
